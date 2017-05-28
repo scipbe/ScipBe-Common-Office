cdc6277f9d4b691cba3acac936ce1f8bd4a9f5cd
Stefan Cruysberghs <Stefan.Cruysberghs@outlook.com>
2017-05-28 09:11:49 +0200

    Fix version number

--- Src/ScipBe.Common.Office/Properties/AssemblyInfo.cs
+++ Src/ScipBe.Common.Office/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ScipBe.Common.Office")]
-[assembly: AssemblyDescription("Wrapper classes for Outlook and OneNote data")]
+[assembly: AssemblyDescription("Wrapper classes for Excel, Outlook and OneNote")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stefan Cruysberghs, http://www.scip.be")]
 [assembly: AssemblyProduct("ScipBe.Common.Office")]
@@ -32,7 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("OfficeTests")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
